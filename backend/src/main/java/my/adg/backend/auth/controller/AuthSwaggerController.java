44d267d8fe1d575881bf89cdb61adbd0411ad3fe
dg.an <dg.an@efit.co.kr>
2025-02-11 13:22:45 +0900

    feat: 토큰 재발급 기능 개발

--- backend/src/main/java/my/adg/backend/auth/controller/AuthSwaggerController.java
+++ backend/src/main/java/my/adg/backend/auth/controller/AuthSwaggerController.java
@@ -3,6 +3,7 @@ package my.adg.backend.auth.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import my.adg.backend.auth.dto.request.LoginRequest;
+import my.adg.backend.auth.dto.request.TokenReissueRequest;
 import my.adg.backend.auth.dto.response.LoginResponse;
 import my.adg.backend.member.dto.request.MemberJoinRequest;
 import my.adg.backend.member.dto.response.MemberFindResponse;
@@ -17,4 +18,8 @@ public interface AuthSwaggerController {
 
     @Operation(summary = "회원가입")
     ResponseEntity<MemberFindResponse> joinMember(MemberJoinRequest request);
+
+    @Operation(summary = "토큰 재발급")
+    ResponseEntity<LoginResponse> reissueToken(TokenReissueRequest request);
+
 }
--- backend/src/main/java/my/adg/backend/auth/service/LoginService.java
+++ backend/src/main/java/my/adg/backend/auth/service/LoginService.java
@@ -2,10 +2,12 @@ package my.adg.backend.auth.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import my.adg.backend.auth.dto.request.LoginRequest;
+import my.adg.backend.auth.dto.request.TokenReissueRequest;
 import my.adg.backend.auth.dto.response.LoginResponse;
 import my.adg.backend.auth.infrastructure.JwtTokenProvider;
 import my.adg.backend.global.exception.BalanceTalkException;
@@ -16,14 +18,13 @@ import my.adg.backend.member.repository.MemberRepository;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class LoginService {
 	private final MemberRepository memberRepository;
 	private final JwtTokenProvider jwtTokenProvider;
 	private final PasswordEncoder passwordEncoder;
 
 	public LoginResponse login(LoginRequest loginRequest) {
-
-		log.info("Login request: {}", loginRequest);
 
 		Member member = memberRepository.findByEmail(loginRequest.email())
 			.orElseThrow(() -> new BalanceTalkException(ErrorCode.MISMATCHED_EMAIL_OR_PASSWORD));
@@ -33,6 +34,15 @@ public class LoginService {
 		if (!matches) {
 			throw new BalanceTalkException(ErrorCode.PASSWORD_MISMATCH);
 		}
+		return LoginResponse.of(member, jwtTokenProvider.createToken(member.getId()));
+	}
+
+	public LoginResponse reissueToken(TokenReissueRequest request) {
+		String memberId = jwtTokenProvider.decodeRefreshToken(request.refreshToken());
+
+		Member member = memberRepository.findById(Long.valueOf(memberId))
+			.orElseThrow(() -> new BalanceTalkException(ErrorCode.NOT_FOUND_MEMBER));
+
 		return LoginResponse.of(member, jwtTokenProvider.createToken(member.getId()));
 	}
 }
